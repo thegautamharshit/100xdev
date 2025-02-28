2d790179a1d6286cb830359253022231a4a7771e
Harshit Gautam <thegautamharshit@gmail.com>
2025-02-28 13:32:26 +0530

    Adding week 7.1 and 7.2

---
+++ class-7/week-7/components/Landing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+    </div>
+  );
+}   
---
+++ class-7/week-7/src/App.jsx
@@ -0,0 +1,150 @@
+// import './App.css'
+
+// import React, { Suspense } from 'react'
+// import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+// // import Landing from '../components/Landing'
+// // import Dashboard from '../components/Dashboard'
+// const Dashboard = React.lazy(() => import('../components/Dashboard'))
+// const Landing = React.lazy(() => import('../components/Landing'))
+// const { defaultAllowedOrigins } = require("vite")
+
+
+// function App() {
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <AppBar />
+//         <Routes>
+//           <Route path="/" element={<Suspense fallback={"Loading..."}><Landing /></Suspense>} />
+//           <Route path="/dashboard" element={<Suspense fallback={"Loading..."}><Dashboard /></Suspense>} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
+
+// const AppBar = React.memo(()=>{
+//   const navigate = useNavigate();
+
+//   const goToDashboard = React.useCallback(()=> navigate ('/dashboard'),[navigate]);
+//   const goToLanding = React.useCallback(()=> navigate ('/'),[navigate]);
+
+//   return <>
+//     <StaticHeader />
+//     <button onClick={()=>{
+//       // window.location.href = '/dashboard'
+//       // navigate('/dashboard')
+//       goToDashboard()
+//     }}><h3>Dashboard</h3></button>
+
+//     <button onClick={()=>{
+//       // window.location.href = '/'
+//       // navigate('/')
+//       goToLanding()
+//     }}><h3>Landing</h3></button>
+//   </>
+// });
+
+// const StaticHeader = React.memo(()=>{
+//   return <h1 style={
+//     {background: "green",position:'fixed',top:0,left:0,right:0}
+//     }>This is Top Bar</h1>
+// })
+
+// export default App
+
+//-----------------------------------------------------------------------------------------
+
+
+// import React, { useContext, useState } from 'react'
+// import { CountContext } from '../components/context';
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <CountContext.Provider value={count}>
+//         <Count count={count} setCount={setCount} /> 
+//       </CountContext.Provider>
+//     </div>
+//   )
+// }
+
+// function Count({setCount}){
+//   return <div>
+//     <CountRender />
+//     <Button setCount={setCount} />
+//   </div>
+// }
+
+// function CountRender(){
+//   const count = useContext(CountContext);
+//   return <div>
+//     <h1>Count: {count}</h1>
+//   </div>
+// }
+
+// function Button({setCount}){
+//   const count = useContext(CountContext);
+//   return <div>
+//     <button onClick={()=>{
+//       setCount(count+1)
+//     }}>Increase</button>
+
+//     <button onClick={()=>{
+//       setCount(count-1)
+//     }}>Decrease</button>
+//   </div>
+// }
+
+// export default App;
+
+
+//--------------------------------------------------------------------------
+
+import './App.css'
+import React from 'react';
+import { RecoilRoot, useRecoilValue, useRecoilState } from 'recoil';
+import { countAtom } from '../store/atoms/count';
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  )
+}
+
+function Count(){
+  return <div>
+    <CountRender />
+    <Button />
+  </div>
+}
+
+function CountRender(){
+  const count = useRecoilValue(countAtom);
+  return <div>
+    <h1>Count: {count}</h1>
+  </div>
+}
+
+function Button(){
+  const [count, setCount] = useRecoilState(countAtom);
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>Increase</button>
+
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>Decrease</button>
+  </div>
+}
+
+export default App;
