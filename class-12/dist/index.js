26bafb7df52b8eeca0b0c9d3ac708218bb46e062
Harshit Gautam <thegautamharshit@gmail.com>
2025-03-24 04:07:25 +0530

    Deleted

--- class-12/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function sumOfAge(user1, user2) {
-    return user1.age + user2.age;
-}
-const age = sumOfAge({ name: 'Harshit Gautam', age: 24 }, { name: "Krishna", age: 25 });
-console.log(age);
